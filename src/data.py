c41e5ca455f1e48c80bb244fd879531788d93135
Brandon I <brandoni@mac.mynetworksettings.com>
2024-11-28 14:53:17 -0500

    Added database for homework completion git push origin main

--- src/data.py
+++ src/data.py
@@ -1,29 +1,30 @@
 import sqlite3
 
 #Create a connection
-conn = sqlite3.connect("attendance.db")
+conn = sqlite3.connect("homework.db")
 
 #Create a cursor
 cur = conn.cursor()
 #Create a table for attendance
-# #cur.execute("""CREATE TABLE 
-#             attendance (Name TEXT, "Week 1" TEXT, "Week 2" TEXT, "Week 3" TEXT, "Week 4" TEXT, 
-#             "Week 5" TEXT, "Week 6" TEXT, "Week 7" TEXT, "Week 8" TEXT, "Week 9" TEXT, "Week 10" TEXT)
-#             """)
+cur.execute("""CREATE TABLE IF NOT EXISTS
+            homework (Name TEXT, "Week 1" TEXT, "Week 2" TEXT, "Week 3" TEXT, "Week 4" TEXT, 
+            "Week 5" TEXT, "Week 6" TEXT, "Week 7" TEXT, "Week 8" TEXT, "Week 9" TEXT, "Week 10" TEXT)
+            """)
 #Insert Sample Data
-# sample_data_to_insert = [
-#     ("Samara", "Yes", "No", "No", "No", "No", "No", "No", "No", "No", "No"),
-#     ("Mazin", "Yes", "Yes", "No", "No", "No", "No", "No", "No", "No", "No"),
-#     ("Yashu", "Yes", "Yes", "Yes", "No", "No", "No", "No", "No", "No", "No"),
-#     ("Deep", "Yes", "No", "No", "Yes", "No", "No", "No", "No", "No", "No"),
-#     ("Brandon", "No", "Yes", "No", "Yes", "No", "No", "No", "No", "No", "No")
-# ]
-# cur.executemany("""INSERT INTO attendance VALUES
-#                 (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-# """, sample_data_to_insert)
-# #Commit and Close
-# conn.commit()
-# conn.close()
+sample_data_to_insert = [
+    ("Samara", "Yes", "No", "No", "No", "No", "No", "No", "No", "No", "No"),
+    ("Mazin", "Yes", "Yes", "No", "No", "No", "No", "No", "No", "No", "No"),
+    ("Yashu", "Yes", "Yes", "Yes", "Yes", "No", "No", "No", "No", "No", "No"),
+    ("Deep", "Yes", "No", "Yes", "Yes", "Yes", "Yes", "No", "No", "No", "No"),
+    ("Brandon", "Yes", "Yes", "No", "Yes", "No", "No", "No", "No", "No", "No")
+]
+cur.executemany("""INSERT INTO homework VALUES
+                (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+""", sample_data_to_insert)
+#Commit and Close
+conn.commit()
+cur.execute("SELECT name FROM sqlite_master WHERE type='table';")
+
 
 
 
