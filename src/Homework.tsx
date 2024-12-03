259990cf7d0e11e681523f5cbbf14d9005d0da16
Samara-dotcom <srahman7@terpmail.umd.edu>
2024-12-03 11:23:29 -0500

    Resolving merge conflict

--- src/Homework.tsx
+++ src/Homework.tsx
@@ -10,7 +10,7 @@ const Homework: React.FC = () => {
     {
       week: 1,
       title: "Homework 1- Git",
-      dueDate: "2024-11-25",
+      dueDate: "2024-11-2",
       instructions: "Complete the git levels on the Google Form.",
       link: "https://forms.gle/MQejHVuzuVzj3uGE7",
       submitted: false
@@ -18,7 +18,7 @@ const Homework: React.FC = () => {
     {
       week: 2,
       title: "Homework 2- More Git and Flexbox",
-      dueDate: "2024-11-25",
+      dueDate: "2024-11-9",
       instructions: "1) Create a new branch titled “flexbox” and incorporate Flexbox into your project 2) Upload a screenshot of Levels 1-6 of “Push & Pull – Git Remotes!” into your new flexbox branch 3) Upload a screenshot of you completing levels 1-13 and 18-20 of Flexbox Froggy into your new flexbox branch",
       link: "https://forms.gle/6XVNWAyVc5ADfZxz9",
       submitted: false
@@ -26,8 +26,8 @@ const Homework: React.FC = () => {
     {
       week: 3,
       title: "Homework 3- Flashcard App",
-      dueDate: "2024-11-25",
-      instructions: "Create a flashcard app! When you click the card, it should show the definition. You can also go to the next card or previous card. If you keep pressing next, it should loop back to the first card and vice-versa. Also, you need to create the ability to add more cards if needed. To get the files go here: https://github.com/KiberVG/bootcamp2024-flashcardhw. Please do not clone the repository. Instead, copy and paste the code into your own code editor or download a zip and get the files that way. When you’re done, make sure to create your own repository for the files",
+      dueDate: "2024-11-16",
+      instructions: "Create a flashcard app! When you click the card, it should show the definition. You can also go to the next card or previous card. If you keep pressing next, it should loop back to the first card and vice-versa. Also, you need to create the ability to add more cards if needed. To get the files go here: https://github.com/KiberVG/bootcamp2024-flashcardhw Please do not clone the repository. Instead, copy and paste the code into your own code editor or download a zip and get the files that way. When you’re done, make sure to create your own repository for the files",
       link: "https://forms.gle/fhbZwTntgESBEY6j8",
       submitted: false,
       image: flashcardImage
@@ -35,8 +35,8 @@ const Homework: React.FC = () => {
     {
       week: 4,
       title: "Homework 4- Matching Cards",
-      dueDate: "2024-11-25",
-      instructions: "Read through https://react.dev/learn/your-first-component, https://react.dev/learn/passing-props-to-a-component, and https://react.dev/learn/conditional-rendering and then complete the challenges for ‘Your first component’ and ‘Conditional Rendering’ and then take screenshots upon completion. Our second set of homework this week is another JavaScript game: https://github.com/KiberVG/MemoryMatchingBootcampHW. It should look like the attached photo when you’re all done.",
+      dueDate: "2024-11-23",
+      instructions: "Read through https://react.dev/learn/your-first-component https://react.dev/learn/passing-props-to-a-component and https://react.dev/learn/conditional-rendering and then complete the challenges for ‘Your first component’ and ‘Conditional Rendering’ and then take screenshots upon completion. Our second set of homework this week is another JavaScript game: https://github.com/KiberVG/MemoryMatchingBootcampHW It should look like the attached photo when you’re all done.",
       link: "https://forms.gle/2bRc93qwD8nwJafu5",
       submitted: false,
       image: matchingImage
@@ -45,7 +45,7 @@ const Homework: React.FC = () => {
       week: 5,
       title: "Homework 5- React and Twitter Clone",
       dueDate: "2024-12-8",
-      instructions: "You do not need to add the functionality to create new tweets or comments, you simply need to display the tweets on the screen and have the ability to like and unlike the tweets. https://github.com/KiberVG/bootcampfall2024-twitterclone. Reading assignments: https://react.dev/learn/rendering-lists (no challenges), https://react.dev/learn/state-a-components-memory (complete challenges), https://react.dev/learn/render-and-commit (no challenges), https://react.dev/learn/updating-objects-in-state (no challenges). It’s important that the reading gets done before Tuesday’s lecture even if you can’t submit the form yet.",
+      instructions: "You do not need to add the functionality to create new tweets or comments, you simply need to display the tweets on the screen and have the ability to like and unlike the tweets. https://github.com/KiberVG/bootcampfall2024-twitterclone Reading assignments: https://react.dev/learn/rendering-lists (no challenges), https://react.dev/learn/state-a-components-memory (complete challenges), https://react.dev/learn/render-and-commit (no challenges), https://react.dev/learn/updating-objects-in-state (no challenges). It’s important that the reading gets done before Tuesday’s lecture even if you can’t submit the form yet.",
       link: "https://forms.gle/65u4Er1kCaEMJioa7",
       submitted: false,
       image: twitterImage
